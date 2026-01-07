b38768e4e80a5cc918a0d32f4dc45d839801ba3e
Marcus Miu <Marcus.Miu27@ycdsbk12.ca>
2026-01-07 11:13:00 -0500

    gampley

---
+++ TESTS/JGraphics.java
@@ -0,0 +1,25 @@
+package TESTS;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class JGraphics extends JPanel{
+    //Variables
+    static int playerX;
+    static int playerY;
+    
+    
+    // Repainting screen based on network input
+    // 
+    public void paintComponent(Graphics g){
+      g.clearRect(0, 0, 1280, 720);
+      g.setColor(Color.BLUE);
+      g.fillRect(playerX, playerX, 100, 100);
+    }
+
+    public JGraphics(){
+      super(); 
+    }
+  }
---
+++ TESTS/gameplay.java
@@ -0,0 +1,35 @@
+package TESTS;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class gameplay implements ActionListener{
+  JGraphics gamePanel;
+  Timer thetimer;
+  String strNetText;
+  
+  // Super Socket Master
+  SuperSocketMaster ssm;
+  
+  public void actionPerformed(ActionEvent evt){
+    if(evt.getSource() == thetimer){
+     gamePanel.repaint(); 
+      
+    }else if(evt.getSource() == ssm){
+      strNetText = ssm.readText();
+      if(strNetText.equals("a")){
+        JGraphics.playerX -= 1;
+      }
+    }
+  
+  public gameplay(){
+    gamePanel = new JGraphics();
+    gamePanel.setLayout(null);
+    gamePanel.setPreferredSize(new Dimension(1280, 720));
+    // SuperSocketMaster server mode
+    ssm = new SuperSocketMaster(6112, this);
+    ssm.connect();
+  } 
+}
